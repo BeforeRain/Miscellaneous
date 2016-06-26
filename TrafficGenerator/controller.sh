873d9ab563516dc1f3442b6f0a3b5126714adbe2
Weiyu Zhang <weiyuzwy@gmail.com>
2016-06-26 23:30:59 +0800

    Start server programs at the very beginning

--- TrafficGenerator/controller.sh
+++ TrafficGenerator/controller.sh
@@ -8,13 +8,23 @@ SERVERS="
 192.168.1.52
 "
 CLIENTS=${SERVERS}
-RESULT_DIR_PREFIX="result"
+
 LOCAL_CONF_FILE="config.txt"
+RESULT_DIR_PREFIX="result"
+
 CONF_FILE="~/TrafficGenerator/conf/config.txt"
+SERVER_PROGRAM="~/TrafficGenerator/bin/server"
+CLIENT_PROGRAM="~/TrafficGenerator/bin/client"
+PORT=5001
 
 
 # Unique directory indentified by timestamp for every round
 result_dir=${RESULT_DIR_PREFIX}_`date +%Y%m%d_%H%M%S`
+
+for server in ${SERVERS}; do
+    # Start server
+    ssh ${USER}@${server} "${SERVER_PROGRAM} -p ${PORT}" >> /dev/null &
+done
 
 for client in ${CLIENTS}; do
     # Create directory to store result for that client
@@ -22,5 +32,4 @@ for client in ${CLIENTS}; do
     # Copy configuration file to client
     scp ${LOCAL_CONF_FILE} ${USER}@${client}:${CONF_FILE}
 done
-
 
